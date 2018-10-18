0dc0d2d1b5f1f3e77e798d3691616550c3436dcf
yairrim <39202018+yairrim@users.noreply.github.com>
2018-10-18 18:07:21 +0300

    Add files via upload

---
+++ HardwareKrakens.java
@@ -0,0 +1,90 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+public class HardwareKrakens
+{
+    /* Public OpMode members. */
+    public DcMotor  MotorLeftFront   = null;
+    public DcMotor  MotorLeftBack  = null;
+    public DcMotor  MotorRightFront   = null;
+    public DcMotor  MotorRightBack  = null;
+    public double TimeToGetDown = 1.0;
+    public double TimeToGoUp = 1.0;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+    private ElapsedTime     runtime = new ElapsedTime();
+
+    /* Constructor */
+    public HardwareKrakens() {}
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        MotorLeftFront  = hwMap.get(DcMotor.class, "mlf");
+        MotorLeftBack   = hwMap.get(DcMotor.class, "mlb");
+        MotorRightFront = hwMap.get(DcMotor.class, "mrf");
+        MotorRightBack  = hwMap.get(DcMotor.class, "mrb");
+
+        MotorLeftFront.setDirection(DcMotor.Direction.FORWARD);
+        MotorLeftBack.setDirection(DcMotor.Direction.FORWARD);
+        MotorRightFront.setDirection(DcMotor.Direction.REVERSE);
+        MotorRightBack.setDirection(DcMotor.Direction.REVERSE);
+
+
+        // Set all motors to zero power
+        AllWheelsPower(0,0);
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        MotorRightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        MotorRightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        MotorLeftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        MotorLeftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    }
+    public void AllWheelsPower(double LeftMotorPower,double RightMotorPower){
+        MotorLeftBack.setPower(LeftMotorPower);
+        MotorLeftFront.setPower(LeftMotorPower);
+        MotorRightBack.setPower(RightMotorPower);
+        MotorRightFront.setPower(RightMotorPower);
+    }
+
+
+}
+
